f6344e8c220c6f88e4a9cd4287d5103d49f8f179
odysseia06 <91327287+odysseia06@users.noreply.github.com>
2022-10-17 14:07:38 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,158 @@
+import socket
+import struct
+import threading
+import queue
+import select
+import time
+import random
+
+class ContinuousClient(threading.Thread):
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.connected = threading.Event()
+        self.alive = threading.Event()
+        self.started = threading.Event()
+        self.alive.set()
+        self.socket = None
+        self.daemon = True
+        self.command_queue = queue.Queue()
+        self.reply_queue = queue.Queue()
+
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            print("Connection attempt.")
+            self.socket.connect((self.host, self.port))
+            print("Connection with server is made.")
+            self.socket.setblocking(0)
+            self.connected.set()
+            if not self.started.is_set():
+                self.start()
+                self.started.set()
+        except ConnectionRefusedError:
+            print("Connection refused.")
+            pass
+        except TimeoutError:
+            print("Timeout error.")
+            pass
+        
+
+    def _recv_n_bytes(self, n, s):
+        """ Convenience method for receiving exactly n bytes from
+            self.socket (assuming it's open and connected).
+        """
+        data = ''
+        while len(data) < n:
+            chunk = s.recv(n - len(data)).decode()
+            if chunk == '':
+                break
+            data += chunk
+        return data
+
+    def run(self):
+        while self.alive.is_set():
+            print(random.randint(0, 100))
+            try:
+                r,_,_ = select.select([self.socket], [], [])
+                if r:
+                    header_data = self._recv_n_bytes(4, self.socket).encode()
+                    if len(header_data) == 4:
+                        msg_len = struct.unpack('<L', header_data)[0]
+                        data = self._recv_n_bytes(msg_len, self.socket)
+                        if len(data) == msg_len:
+                            self.reply_queue.put(data)
+            except Exception as e:
+                self.connected.clear()
+                #print(e)
+                
+
+    def send(self, message):
+        try:
+            header = struct.pack('<L', len(message))
+            self.socket.sendall(header + message.encode())
+        except:
+            pass
+
+    def join(self, timeout=None):
+            self.alive.clear()
+            threading.Thread.join(self, timeout)
+
+        
+class DisposableClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        
+    def _recv_n_bytes(self, n, s):
+        """ Convenience method for receiving exactly n bytes from
+            self.socket (assuming it's open and connected).
+        """
+        data = ''
+        while len(data) < n:
+            chunk = s.recv(n - len(data)).decode()
+            if chunk == '':
+                break
+            data += chunk
+        return data
+
+    def connect_and_get(self, message, delay=None):
+        if delay:
+            time.sleep(delay)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        header = struct.pack('<L', len(message))
+        try:
+            s.sendall(header + message.encode())
+
+            header_data = self._recv_n_bytes(4, s).encode()
+            if len(header_data) == 4:
+                msg_len = struct.unpack('<L', header_data)[0]
+                data = self._recv_n_bytes(msg_len, s)
+                if len(data) == msg_len:
+                    return data
+        except IOError as e:
+            print(str(e))
+            return None
+
+        s.shutdown()
+
+
+def disposable_client_send_loop():
+    ds = DisposableClient("localhost", 50000)
+    count = 0
+    while True:
+        try:
+            print(ds.connect_and_get("Hello"+str(count)))
+            count += 1
+        except KeyboardInterrupt:
+            break
+            
+def continuous_client_send_loop():
+    cs = ContinuousClient("localhost", 50000)
+    count = 0
+    cs.connect()
+    while True:
+        try:
+            cs.send("Hello"+str(count))
+            count += 1
+            print(cs.reply_queue.get())
+        except KeyboardInterrupt:
+            break
+
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=continuous_client_send_loop, args=())
+    t2 = threading.Thread(target=continuous_client_send_loop, args=())
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+        
---
+++ server.py
@@ -0,0 +1,112 @@
+import select
+import socket
+import struct
+import queue
+import threading
+import time
+
+class SocketServer(threading.Thread):
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.port = port
+        self.sock = None
+        self.inputs = []
+        self.outputs = []
+        self.message_queues = {}
+        self.online = threading.Event()
+        self.daemon = True
+
+    def startServer(self):
+        if not self.online.is_set():
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setblocking(0)
+            self.sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sock.bind((self.host, self.port))
+            self.sock.listen(10)
+            self.inputs.append(self.sock)
+            self.online.set()
+            print("Server is online.")
+            self.start()
+
+    def _recv_n_bytes(self, n, s):
+        """ Convenience method for receiving exactly n bytes from
+            self.socket (assuming it's open and connected).
+        """
+        data = ''
+        while len(data) < n:
+            chunk = s.recv(n - len(data)).decode()
+            if chunk == '':
+                break
+            data += chunk
+        return data
+
+    def receive(self, s):
+        try:
+            header_data = self._recv_n_bytes(4, s).encode()
+            if len(header_data) == 4:
+                msg_len = struct.unpack('<L', header_data)[0]
+                data = self._recv_n_bytes(msg_len, s)
+                if len(data) == msg_len:
+                    print("Received %s from %s" %(data, s.getpeername()))
+                    self.message_queues[s].put(header_data.decode() + data)
+                    if s not in self.outputs:
+                        self.outputs.append(s)
+
+                    #self.inputs.remove(s) #Remove disposable client sockets for now
+                    return
+            else:
+                if s in self.outputs:
+                    self.outputs.remove(s)
+                    s.close()
+                    del self.message_queues[s]
+        except IOError as e:
+            pass
+
+    def run(self):
+        while self.inputs:
+            readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs)
+            for s in writable:
+                try:
+                    next_msg = self.message_queues[s].get_nowait()
+                except queue.Empty:
+                    self.outputs.remove(s)
+                except KeyError: #HANDLE IT LATER
+                    pass
+                else:
+                    s.sendall(next_msg.encode())   #Works like a echo-server for now
+                    #self.outputs.remove(s) #Remove disposable client sockets for now
+
+            for s in exceptional:
+                print("Exception for %s" %(s.getpeername()))
+                self.inputs.remove(s)
+                if s in self.outputs:
+                    self.outputs.remove(s)
+                s.close()
+                del self.message_queues[s]
+
+            for s in readable:
+                if s is self.sock:
+                    conn, addr = s.accept()
+                    conn.setblocking(0)
+                    self.inputs.append(conn)
+                    self.message_queues[conn] = queue.Queue()
+
+                else:
+                    self.receive(s)
+            
+
+            if not self.online.is_set():
+                self.inputs.remove(self.sock)
+                self.sock.close()
+                break
+
+
+if __name__ == "__main__":
+    server = SocketServer("localhost", 50000)
+    server.startServer()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
